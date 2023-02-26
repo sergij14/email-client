fff9cd3abbd4f05d81f246437a788e022168d01a
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-26 20:27:06 +0400

    created signin initial functionality & markup

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-z0-9]+$/i),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+  });
 
+  onSubmit() {
+    if (this.form.invalid) {
+      Object.keys(this.form.controls).forEach((field) =>
+        this.form.get(field)?.markAsDirty()
+      );
+      return;
+    }
+  }
 }
