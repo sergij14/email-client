dfddd28b8f0a5af43ad756fbe6b86778ababff58
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-26 13:18:44 +0400

    setup routing for auth module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: SigninComponent,
+    path: '',
+  },
+  {
+    component: SignupComponent,
+    path: 'signup',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
