7363d62d9b7f079cb6780f23b7ecc195f7598a85
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-27 13:17:31 +0400

    extracting url params from single email route

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-email-show',
   templateUrl: './email-show.component.html',
-  styleUrls: ['./email-show.component.css']
+  styleUrls: ['./email-show.component.css'],
 })
-export class EmailShowComponent {
+export class EmailShowComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    console.log(
+      this.route.params.subscribe(({ id }) => {
+        console.log(id);
+      })
+    );
+  }
 }
