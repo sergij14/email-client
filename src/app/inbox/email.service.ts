ca0995a0520f41c2493c53c3f00f5a10282a1c11
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-27 09:22:12 +0400

    added email list fetching to email service

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  rootURL = 'https://api.angular-email.com';
+
+  getEmails() {
+    return this.httpClient.get<EmailSummary[]>(`${this.rootURL}/emails`, {
+      withCredentials: true,
+    });
+  }
 }
