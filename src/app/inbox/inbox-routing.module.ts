55f93004aaa20f80b795f72e42687177ab4a0f7d
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-27 09:59:18 +0400

    showing placeholder or specific email with child routes

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmailShowComponent } from './email-show/email-show.component';
 import { HomeComponent } from './home/home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: PlaceholderComponent,
+      },
+      { path: ':id', component: EmailShowComponent },
+    ],
   },
 ];
 
