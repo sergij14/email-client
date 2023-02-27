d5b6e48c3c2337b4d140db4231365b3a8af2975d
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-27 18:36:11 +0400

    basic setup for email reply component

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Email } from '../email';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  styleUrls: ['./email-reply.component.css'],
 })
 export class EmailReplyComponent {
+  modalVisible = false;
+  email!: Email;
 
+  constructor(
+    private authService: AuthService,
+    private emailService: EmailService
+  ) {
+    this.email = {
+      id: '',
+      to: '',
+      from: `${authService.username}@angular-email.com`,
+      html: '',
+      text: '',
+      subject: '',
+    };
+  }
+
+  showModal() {
+    this.modalVisible = true;
+  }
+
+  hideModal() {
+    this.modalVisible = false;
+  }
+
+  onEmailSubmit(email: Email) {}
 }
